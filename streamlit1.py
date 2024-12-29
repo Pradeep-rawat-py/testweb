886eab2068a5eeb1e9f14057f8278bc4891e53c9
Pradeep-rawat-py <114752116+Pradeep-rawat-py@users.noreply.github.com>
2024-12-29 14:15:12 +0530

    done

---
+++ streamlit1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.image(r"C:\Users\Pradeep Singh Rawat\Pictures\Camera Roll\WIN_20230625_21_04_40_Pro.jpg")
+
+st.title("Fist Website")
+
+st.header("Welcome to Data Analysis")
+
+st.subheader("Data Manipulation")
+
+st.info("This page will guide you how you can win in Data analysis field")
+
+st.warning("Do not share or use your password")
+
+st.write("Here is the Introduction")
+
+st.error("This website may give you errors")
+
+st.success("Taskcompleted")
+
+st.markdown("*streamlit*")
+
+st.caption("Welcome")
+
+st.latex(r"a + bx^3 + c")
