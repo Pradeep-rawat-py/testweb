37ce1d555a414fe2cbf77d8f08ec970bba7c9f69
Pradeep-rawat-py <114752116+Pradeep-rawat-py@users.noreply.github.com>
2024-12-29 15:18:44 +0530

    New done

--- streamlit1.py
+++ streamlit1.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import time as t
 
+st.title("HACKED")
+
+st.header("hacking device.....")
+
+progress_bar = st.progress(0)
+st.caption("Copying data")
+
+for i in range(0, 100):
+    t.sleep(0.05)
+    progress_bar.progress(i)
+
+st.success("Device hacked")
+
+
+with st.spinner("Wait"):
+    t.sleep(5)
 
 st.title("Fist Website")
 
@@ -22,3 +39,17 @@ st.markdown("*streamlit*")
 st.caption("Welcome")
 
 st.latex(r"a + bx^3 + c")
+
+with st.sidebar:
+    st.title("Abc")
+    
+    
+    
+st.sidebar.text_input("Enter your name")
+st.sidebar.text_input("Enter your password")
+st.sidebar.button("Submit")
+
+
+
+st.markdown("🤖 Congratulations")
+st.balloons()
